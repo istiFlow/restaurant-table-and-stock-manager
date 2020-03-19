8e7e130fb3220d050d1bd2732c980b9a5a659830
Istvan Dumitras <d.steven@protonmail.ch>
2020-03-19 11:50:53 +0100

    created an entity  for stock

--- src/main/java/hu/restaurant/Restaurant/Entity/Stock.java
+++ src/main/java/hu/restaurant/Restaurant/Entity/Stock.java
@@ -6,7 +6,10 @@ import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.time.LocalDate;
@@ -17,30 +20,18 @@ import java.util.List;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name = "users")
-public class User {
+@Table(name = "stock")
+public class Stock {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(unique = true, length = 50)
-    private String username;
-
-    @Transient
-    @JsonIgnore
-    private String password;
+    @Column
+    private String commodity;
 
     @Column
-    private String firstName;
-    @Column
-    private String lastName;
-
-    @Column(unique = true)
-    private String email;
-
-    @Enumerated(EnumType.STRING)
-    private Enum Role;
+    private Integer weight;
 
     @Column
     @JsonDeserialize(using = LocalDateDeserializer.class)
@@ -48,16 +39,7 @@ public class User {
     @JsonFormat(pattern = "yyyy-MM-dd")
     private LocalDate loginDate;
 
-    @OneToMany(cascade = CascadeType.ALL, mappedBy = "user")
-    @JsonIgnore
-    private List<Order> orders;
-
-    // for oauth2
-   /* @Transient
-    @JsonIgnore
-    @Override
-    public String getName() {
-        return username;
-    }*/
+    @ManyToOne
+    private Order order;
 
 }

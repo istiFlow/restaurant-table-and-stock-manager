7eeed8c1befd5ba9115a8f07ac1df349c30b4395
Istvan Dumitras <d.steven@protonmail.ch>
2020-03-22 16:18:36 +0100

    controller has been added

---
+++ src/main/java/hu/restaurant/Restaurant/Controller/UserController.java
@@ -0,0 +1,11 @@
+package hu.restaurant.Restaurant.Controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+@AllArgsConstructor
+public class UserController {
+}

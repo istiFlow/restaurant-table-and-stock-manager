66c0f92cbf0f7980b3c9e50f2b0a13ccdf2a15f2
Istvan Dumitras <d.steven@protonmail.ch>
2020-03-26 16:32:13 +0100

    Modifying deleteByIdIn for users to delete them in bulk

--- src/main/java/hu/restaurant/Restaurant/Repository/UserRepository.java
+++ src/main/java/hu/restaurant/Restaurant/Repository/UserRepository.java
@@ -2,8 +2,14 @@ package hu.restaurant.Restaurant.Repository;
 
 import hu.restaurant.Restaurant.Entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface UserRepository extends JpaRepository<User,Long> {
+
+    @Modifying
+    void deleteByIdIn(List<Long> id);
 }

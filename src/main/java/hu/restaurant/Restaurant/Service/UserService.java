ba39fa0f1548dffb4e187454a94ed68759971589
Istvan Dumitras <d.steven@protonmail.ch>
2020-03-26 16:29:22 +0100

    created a service for crud

--- src/main/java/hu/restaurant/Restaurant/Service/UserService.java
+++ src/main/java/hu/restaurant/Restaurant/Service/UserService.java
@@ -1,12 +1,14 @@
 package hu.restaurant.Restaurant.Service;
 
 import hu.restaurant.Restaurant.Entity.User;
+import hu.restaurant.Restaurant.Exception.UserNotFoundException;
 import hu.restaurant.Restaurant.Repository.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
@@ -15,7 +17,30 @@ public class UserService {
 
     private UserRepository userRepository;
 
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public Optional<User> findOne(Long id) {
+        return Optional.of(userRepository.findById(id))
+                .orElseThrow(() -> new UserNotFoundException(id));
+    }
+
     public List<User> findAll() {
         return userRepository.findAll();
     }
+
+    public User updateUserDetails(Long id, User user) {
+        user.setId(id);
+        return userRepository.save(user);
+    }
+
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
+
+    public void deleteMoreUsers(List<Long> id) {
+        userRepository.deleteByIdIn(id);
+    }
+
 }
